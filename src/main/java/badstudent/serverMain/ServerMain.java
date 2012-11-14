8f603a842adba3560979bb7d00b01e4661ca0afd
matYang <uwse@me.com>
2012-11-14 00:14:26 -0500

    Revert "Revert "the skeleton backend Java maven project""
    This reverts commit c216c926285d6b4b82e2b0f7a22ce8de5922446d.

---
+++ src/main/java/badstudent/serverMain/ServerMain.java
@@ -0,0 +1,77 @@
+package badstudent.serverMain;
+
+import org.restlet.Component;
+import org.restlet.Server;
+import org.restlet.data.Protocol;
+
+
+public class ServerMain {
+
+	//private static Log log = LogFactory.getLog(ServiceMain.class);
+
+	private static ServerMain me;
+
+	private Component component;
+
+	public void init(String[] arguments) {
+
+	}
+
+	/**
+	 * Start the Thread, accept incoming connections
+	 * 
+	 * Use this entry point to start with embedded HTTP Server
+	 * 
+	 * @throws Exception
+	 */
+	public void start() throws Exception {
+		component = new Component();
+
+		// Add a new HTTP server listening on port
+
+		Server server = component.getServers().add(Protocol.HTTP, 8015);
+		server.getContext().getParameters().add("maxThreads", "128");
+
+		// Attach the sample application.
+
+		//MessageService messageService = new MessageService();
+
+		//component.getDefaultHost().attach(messageService);
+
+		// Start the component.
+		//log.info("ready to start");
+		System.out.println("ready to start");
+		component.start();
+
+	}
+
+	/**
+	 * Stops RESTlet application
+	 */
+	public void stop() {
+		component.getDefaultHost().detach(component.getApplication());
+	}
+
+	public static ServerMain getInstance() {
+		if (me == null) {
+			me = new ServerMain();
+		}
+
+		return me;
+	}
+	
+
+
+	public static void main(String... args) throws Exception {
+		System.out.println("Excuting");
+		// Load server logic
+		try {
+			ServerMain.getInstance().init(args);
+			ServerMain.getInstance().start();
+		} catch (Exception e) {
+			//log.error("Failed to start server", e);
+		}
+		
+	}
+
+}

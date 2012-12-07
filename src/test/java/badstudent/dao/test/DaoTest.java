efea0f3632013be11b2610cd735c87b83e90ca78
michael.xiao <michael.xiao@xtremelabs.com>
2012-12-07 12:38:56 -0500

    test update

--- src/test/java/badstudent/dao/test/DaoTest.java
+++ src/test/java/badstudent/dao/test/DaoTest.java
@@ -15,7 +15,6 @@ import org.junit.Test;
 
 public class DaoTest {
 	
-	DaoMessage dao = new DaoMessage();
 	Jedis jedis = new Jedis("localhost");
 	
 	@Test
@@ -23,33 +22,33 @@ public class DaoTest {
 		Location locationUW = new Location("江苏省", "南京市", "南京农业大学");
 	    Message msgUW = new Message("Simon","lol","2012 12 21","2012 12 21",30,locationUW,1,"looking for girlfriend","simon@uwaterloo.ca",
 	            "519xxxxxx","123456789","twit","SimonJiang", 19.99, 1);
-	    dao.addMessageToDatabase(msgUW);
+	    DaoMessage.addMessageToDatabase(msgUW);
 	    Location locationUL = new Location("江苏省", "南京市", "南京农业大学");
 	    Message msgUL = new Message("Simon","lol","2012 12 22","2012 12 22",30,locationUL,0,"looking for girlfriend","simon@uwaterloo.ca",
 	            "519xxxxxx","123456789","twit","SimonJiang", 19.99, 0);
-	    dao.addMessageToDatabase(msgUL);
+	    DaoMessage.addMessageToDatabase(msgUL);
 	    Location locationUT = new Location("江苏省", "南京市", "南京农业大学");
 	    Message msgUT = new Message("Simon","lol","2012 12 23","2012 12 23",30,locationUT,2,"looking for girlfriend","simon@uwaterloo.ca",
 	            "519xxxxxx","123456789","twit","SimonJiang", 19.99, -1);
-	    dao.addMessageToDatabase(msgUT);
+	    DaoMessage.addMessageToDatabase(msgUT);
 
 	    assertTrue(Integer.parseInt(jedis.get(Constants.key_idGenerator)) == 3);
 	    
-	    Message returnMsgUW = dao.getMessageById(msgUW.getId());
-	    Message returnMsgUT = dao.getMessageById(msgUT.getId());
-	    Message returnMsgUL = dao.getMessageById(msgUL.getId());
+	    Message returnMsgUW = DaoMessage.getMessageById(msgUW.getId());
+	    Message returnMsgUT = DaoMessage.getMessageById(msgUT.getId());
+	    Message returnMsgUL = DaoMessage.getMessageById(msgUL.getId());
 	    Common.d("daoTest::initDaoTest() -> initial msgUW location " + msgUW.getLocation());
 	    Common.d("daoTest::initDaoTest() -> retuerned msgUW location " + returnMsgUW.getLocation());
 	    assertTrue(returnMsgUW.getLocation().toString().compareTo(msgUW.getLocation().toString()) == 0);
 	    assertTrue(returnMsgUT.getLocation().toString().compareTo(msgUT.getLocation().toString()) == 0);
 	    assertTrue(returnMsgUL.getLocation().toString().compareTo(msgUL.getLocation().toString()) == 0);
 	    
-	    dao.deleteMessageFromDatabase(msgUT.getId());
-	    dao.deleteMessageFromDatabase(msgUW.getId());
-	    dao.deleteMessageFromDatabase(msgUL.getId());
+	    DaoMessage.deleteMessageFromDatabase(msgUT.getId());
+	    DaoMessage.deleteMessageFromDatabase(msgUW.getId());
+	    DaoMessage.deleteMessageFromDatabase(msgUL.getId());
 	    assertTrue(Integer.parseInt(jedis.get(Constants.key_idGenerator)) == 3);
 	    
-	    List<Message> msgs = dao.getAllMessagesInDatabase();
+	    List<Message> msgs = DaoMessage.getAllMessagesInDatabase();
 		assertTrue(msgs.size() == 0);				//make sure the database is totally cleared, no memory has occurred in the above operations
 		
 		jedis.del(Constants.key_idGenerator);
@@ -62,16 +61,16 @@ public class DaoTest {
 	public void createMessageTest() {
 		Message msg = new Message("Matthew");
 		
-		dao.addMessageToDatabase(msg);
+		DaoMessage.addMessageToDatabase(msg);
 		String msgId = msg.getId();
-		assertNotNull(dao.getMessageById(msgId));
+		assertNotNull(DaoMessage.getMessageById(msgId));
 		
-		dao.deleteMessageFromDatabase(msgId);
-		assertNull(dao.getMessageById(msgId));
+		DaoMessage.deleteMessageFromDatabase(msgId);
+		assertNull(DaoMessage.getMessageById(msgId));
 		
 		assertNull(jedis.get(msgId));
 		
-		List<Message> msgs = dao.getAllMessagesInDatabase();
+		List<Message> msgs = DaoMessage.getAllMessagesInDatabase();
 		assertTrue(msgs.size() == 0);				//make sure the database is totally cleared, no memory has occurred in the above operations
 	}
 	
@@ -79,18 +78,18 @@ public class DaoTest {
 	@Test
 	public void getMessageByIdentifierTest() {
 		Message message = new Message("Michael");
-		dao.addMessageToDatabase(message);
+		DaoMessage.addMessageToDatabase(message);
 		
-		Message returned = dao.getMessageById(message.getId());
+		Message returned = DaoMessage.getMessageById(message.getId());
 		
 		assertNotNull(returned);
 		assertTrue(returned.getId().compareTo(message.getId()) == 0);
 		
 		jedis.del(message.getId());
-		Message newReturned = dao.getMessageById(message.getId());
+		Message newReturned = DaoMessage.getMessageById(message.getId());
 		
 		assertNull(newReturned);
-		List<Message> messages = dao.getAllMessagesInDatabase();
+		List<Message> messages = DaoMessage.getAllMessagesInDatabase();
 		assertTrue(messages.size() == 0);			//make sure the database is totally cleared, no memory has occurred in the above operations
 	}
 	
@@ -100,19 +99,19 @@ public class DaoTest {
 		
 		//here "test" is the original roomNumber
 		Message message = new Message("MichaelMatthew");
-		dao.addMessageToDatabase(message);
+		DaoMessage.addMessageToDatabase(message);
 
 		message.setUserName("jUnit");
-		dao.updateMessageInDatabase(message);
+		DaoMessage.updateMessageInDatabase(message);
 		
-		Message returned = dao.getMessageById(message.getId());
+		Message returned = DaoMessage.getMessageById(message.getId());
 		
 		assertTrue(returned.getUserName().compareTo("jUnit") == 0);  //check if updated
 		
 		jedis.del(message.getId());
-		assertNull(dao.getMessageById(message.getId()));   //check if new message has been deleted
+		assertNull(DaoMessage.getMessageById(message.getId()));   //check if new message has been deleted
 		
-		List<Message> messages = dao.getAllMessagesInDatabase();
+		List<Message> messages = DaoMessage.getAllMessagesInDatabase();
 		assertTrue(messages.size() == 0);
 	}
 	
@@ -120,19 +119,19 @@ public class DaoTest {
 	public void deleteMessageTest() {
 		Message message = new Message("admin");
 		
-		dao.addMessageToDatabase(message);
+		DaoMessage.addMessageToDatabase(message);
 		
 		String messageIdentifier = message.getId();
 		
 		assertNotNull(jedis.get(messageIdentifier));
 		
-		Message returned = dao.getMessageById(message.getId());
+		Message returned = DaoMessage.getMessageById(message.getId());
 		assertTrue(returned.getId().compareTo(message.getId()) ==0);	//make sure the message is in the database
 		
-		dao.deleteMessageFromDatabase(message.getId());
-		assertNull(dao.getMessageById(messageIdentifier));		//make sure the message now is being deleted
+		DaoMessage.deleteMessageFromDatabase(message.getId());
+		assertNull(DaoMessage.getMessageById(messageIdentifier));		//make sure the message now is being deleted
 		
-		List<Message> messages = dao.getAllMessagesInDatabase();
+		List<Message> messages = DaoMessage.getAllMessagesInDatabase();
 		assertTrue(messages.size() == 0);				//make sure the database is totally cleared, no memory has occurred in the above operations
 	}
 	
@@ -142,23 +141,23 @@ public class DaoTest {
 		Message messageBeta = new Message("SunnyMatthew");
 		Message messageSigma = new Message("SunnyEmma");
 		
-		dao.addMessageToDatabase(messageAlpha);
-		dao.addMessageToDatabase(messageBeta);
-		dao.addMessageToDatabase(messageSigma);
+		DaoMessage.addMessageToDatabase(messageAlpha);
+		DaoMessage.addMessageToDatabase(messageBeta);
+		DaoMessage.addMessageToDatabase(messageSigma);
 		Common.d( "daoTest::getCurrentMessagesTest -> retuerned messagealpha" + jedis.get(messageAlpha.getId()) );
 		Common.d( "daoTest::getCurrentMessagesTest -> retuerned messagebeta" + jedis.get(messageBeta.getId()) );
 		Common.d( "daoTest::getCurrentMessagesTest -> retuerned messagesigma" + jedis.get(messageSigma.getId()) );
 		
 		
-		List<Message> messages = dao.getAllMessagesInDatabase();
+		List<Message> messages = DaoMessage.getAllMessagesInDatabase();
 		Common.d( "daoTest::getMessagesTest -> retuerned messages size" + messages.size() );
 		assertTrue(messages.size() == 3);
 		
-		dao.deleteMessageFromDatabase(messageAlpha.getId());      //redis duplicate keys problem
-		dao.deleteMessageFromDatabase(messageBeta.getId());
-		dao.deleteMessageFromDatabase(messageSigma.getId());
+		DaoMessage.deleteMessageFromDatabase(messageAlpha.getId());      //redis duplicate keys problem
+		DaoMessage.deleteMessageFromDatabase(messageBeta.getId());
+		DaoMessage.deleteMessageFromDatabase(messageSigma.getId());
 		
-		messages = dao.getAllMessagesInDatabase();
+		messages = DaoMessage.getAllMessagesInDatabase();
 		assertTrue(messages.size() == 0);
 		jedis.flushDB();
 	}

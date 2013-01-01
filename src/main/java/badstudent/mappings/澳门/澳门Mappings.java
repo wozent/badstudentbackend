b6cadf7996a12ce8ee2f50d93d18f266897b1dc3
michael.xiao <michael.xiao@xtremelabs.com>
2013-01-01 12:56:15 -0500

    Location data[complete]

---
+++ src/main/java/badstudent/mappings/澳门/澳门Mappings.java
@@ -0,0 +1,15 @@
+package badstudent.mappings.澳门;
+
+import badstudent.mappings.MappingBase;
+import badstudent.mappings.澳门.澳门特别行政区.澳门特别行政区Mappings;
+
+public class 澳门Mappings extends MappingBase {
+
+    @Override
+    protected void initMappings() {
+        subAreaMappings.put("澳门特别行政区",new 澳门特别行政区Mappings());
+
+
+    }
+
+}
---
+++ src/main/java/badstudent/mappings/澳门/澳门特别行政区/澳门特别行政区Mappings.java
@@ -0,0 +1,15 @@
+package badstudent.mappings.澳门.澳门特别行政区;
+
+import badstudent.mappings.MappingBase;
+import badstudent.mappings.澳门.澳门特别行政区.澳门区.澳门区Mappings;
+
+public class 澳门特别行政区Mappings extends MappingBase {
+
+    @Override
+    protected void initMappings() {
+        subAreaMappings.put("澳门区",new 澳门区Mappings());
+
+
+    }
+
+}
